a2603ac5e1b4996c72887055b78e391ed608f4a8
Evgeniy <51084295+LapEv@users.noreply.github.com>
2026-04-30 21:47:10 +0300

    check ts error

---
+++ packages/client/src/pages/ControlRoom/Incidents/Utils/index.ts
@@ -0,0 +1,4 @@
+export { getInitialSettings } from './GetInitialSettings'
+export { GetIndicatorData } from './GetIndicatorData'
+export { GetActiveColumnOptions } from './GetActiveColumnOptions'
+export { CheckOperators } from './CheckOperators'
